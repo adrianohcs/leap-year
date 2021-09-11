0e1244b029d0d6a355510b586305ee733f754e76
Adriano <adriano.henrique.eng@gmail.com>
2021-09-11 20:40:21 -0300

    Added the solution through else-if statements.

--- src/leapyear/LeapYearCheck.java
+++ src/leapyear/LeapYearCheck.java
@@ -5,11 +5,18 @@ import java.util.Scanner;
 /*
 A year is a leap year if it is divisible by 4. However, if the year is divisible by 100, then it is a leap year only when it is also divisible by 400.
  */
-public class IsItALeapYear {
+public class LeapYearCheck {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Give a year: ");
         int year = Integer.parseInt(scanner.nextLine());
-        System.out.println(year);
+        System.out.println("Is " + year + " a leap year?");
+        if (year % 4 != 0) {
+            System.out.println(year + " is not a leap year.");
+        } else if (year % 100 == 0 && year % 400 != 0) {
+            System.out.println(year + " is not a leap year.");
+        } else {
+            System.out.println(year + " is a leap year");
+        }
     }
 }
